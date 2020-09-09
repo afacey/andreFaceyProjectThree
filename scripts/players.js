08e8eac13f4b205f32952aa7d0e7f089ff7e8fc3
Andre <drefacey@gmail.com>
2020-09-09 18:48:51 -0400

    refactor get random names method to include answer in random position

--- scripts/players.js
+++ scripts/players.js
@@ -14,36 +14,37 @@ const players = [
   {
     name: "Dewan Hernandez",
     imgSrc: "../assets/dewanHernandez.png"
-  },
-  {
-    name: "Fred VanVleet",
-    imgSrc: "../assets/fredVanvleet.png"
-  },{
-    name: "Malcolm Miller",
-    imgSrc: "../assets/malcolmMiller.png"
-  },{
-    name: "Matt Thomas",
-    imgSrc: "../assets/mattThomas.png"
-  },{
-    name: "Norman Powell",
-    imgSrc: "../assets/normanPowell.png"
-  },{
-    name: "Pascal Siakam",
-    imgSrc: "../assets/pascalSiakam.png"
-  },{
-    name: "Patrick McCaw",
-    imgSrc: "../assets/patrickMccaw.png"
-  },{
-    name: "Rondae Hollis-Jefferson",
-    imgSrc: "../assets/rondaeHollisJefferson.png"
-  },{
-    name: "Serge Ibaka",
-    imgSrc: "../assets/sergeIbaka.png"
-  },{
-    name: "Stanley Johnson",
-    imgSrc: "../assets/stanleyJohnson.png"
-  },{
-    name: "Terence Davis",
-    imgSrc: "../assets/terenceDavis.png"
-  },
+  }
+  // ,
+  // {
+  //   name: "Fred VanVleet",
+  //   imgSrc: "../assets/fredVanvleet.png"
+  // },{
+  //   name: "Malcolm Miller",
+  //   imgSrc: "../assets/malcolmMiller.png"
+  // },{
+  //   name: "Matt Thomas",
+  //   imgSrc: "../assets/mattThomas.png"
+  // },{
+  //   name: "Norman Powell",
+  //   imgSrc: "../assets/normanPowell.png"
+  // },{
+  //   name: "Pascal Siakam",
+  //   imgSrc: "../assets/pascalSiakam.png"
+  // },{
+  //   name: "Patrick McCaw",
+  //   imgSrc: "../assets/patrickMccaw.png"
+  // },{
+  //   name: "Rondae Hollis-Jefferson",
+  //   imgSrc: "../assets/rondaeHollisJefferson.png"
+  // },{
+  //   name: "Serge Ibaka",
+  //   imgSrc: "../assets/sergeIbaka.png"
+  // },{
+  //   name: "Stanley Johnson",
+  //   imgSrc: "../assets/stanleyJohnson.png"
+  // },{
+  //   name: "Terence Davis",
+  //   imgSrc: "../assets/terenceDavis.png"
+  // },
 ]
