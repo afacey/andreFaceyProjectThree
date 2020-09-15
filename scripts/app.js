8ff680ef5f407b733f6441aaba2e038e4f4a3800
Andre <drefacey@gmail.com>
2020-09-14 20:27:03 -0400

    small changes for project submission

--- scripts/app.js
+++ scripts/app.js
@@ -5,27 +5,22 @@ const playerData = [
   { name: "Dewan Hernandez", imgSrc: "./assets/dewanHernandez.png"},
   { name: "Fred VanVleet", imgSrc: "./assets/fredVanvleet.png"},
   { name: "Kyle Lowry", imgSrc: "./assets/kyleLowry.png" },
-  // { name: "Malcolm Miller", imgSrc: "./assets/malcolmMiller.png" },
-  // { name: "Marc Gasol", imgSrc: "./assets/marcGasol.png" },
-  // { name: "Matt Thomas", imgSrc: "./assets/mattThomas.png" },
-  // { name: "Norman Powell", imgSrc: "./assets/normanPowell.png"},
-  // { name: "OG Anunoby", imgSrc: "./assets/ogAnunoby.png" },
-  // { name: "Oshae Brissett", imgSrc: "./assets/oshaeBrissett.png" },
-  // { name: "Pascal Siakam", imgSrc: "./assets/pascalSiakam.png"},
-  // { name: "Patrick McCaw", imgSrc: "./assets/patrickMccaw.png" },
-  // { name: "Rondae Hollis-Jefferson", imgSrc: "./assets/rondaeHollisJefferson.png" },
-  // { name: "Serge Ibaka", imgSrc: "./assets/sergeIbaka.png" },
-  // { name: "Stanley Johnson", imgSrc: "./assets/stanleyJohnson.png" },
-  // { name: "Terence Davis", imgSrc: "./assets/terenceDavis.png"}
+  { name: "Malcolm Miller", imgSrc: "./assets/malcolmMiller.png" },
+  { name: "Marc Gasol", imgSrc: "./assets/marcGasol.png" },
+  { name: "Matt Thomas", imgSrc: "./assets/mattThomas.png" },
+  { name: "Norman Powell", imgSrc: "./assets/normanPowell.png"},
+  { name: "OG Anunoby", imgSrc: "./assets/ogAnunoby.png" },
+  { name: "Oshae Brissett", imgSrc: "./assets/oshaeBrissett.png" },
+  { name: "Pascal Siakam", imgSrc: "./assets/pascalSiakam.png"},
+  { name: "Patrick McCaw", imgSrc: "./assets/patrickMccaw.png" },
+  { name: "Rondae Hollis-Jefferson", imgSrc: "./assets/rondaeHollisJefferson.png" },
+  { name: "Serge Ibaka", imgSrc: "./assets/sergeIbaka.png" },
+  { name: "Stanley Johnson", imgSrc: "./assets/stanleyJohnson.png" },
+  { name: "Terence Davis", imgSrc: "./assets/terenceDavis.png"}
 ]
 
 // ===================================================== NAMESPACE
 const raptorsQuiz = {};
-
-// TODO remove console.logs for project submission
-
-// EXTRAS
-// TODO add score reactions
 
 // ===================================================== NAMESPACE > VARIABLES
 raptorsQuiz.playerData = playerData; // store external player data
